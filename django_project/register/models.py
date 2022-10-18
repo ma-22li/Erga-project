a8852cf24f072de59685ce75410e5a163070d90e
salwa alqurashi <92172130+Salwaq@users.noreply.github.com>
2022-10-17 23:11:10 -0700

    done models and creat api folder

--- django_project/register/models.py
+++ django_project/register/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username= models.CharField(max_length=50)
+    email= models.EmailField(max_length= 254, help_text='example@email.com')
+    password= models.CharField(max_length= 50)
+    city= models.CharField(max_length=50)
