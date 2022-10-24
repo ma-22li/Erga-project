3705a40fb7438bfb010a2f6d53acc3e4cf15986f
Waad Alabdualjabbar <waad.1999x@gmail.com>
2022-10-24 19:39:02 +0300

    mobile frontend created

--- flutter_project/lib/sign_up.dart
+++ flutter_project/lib/sign_up.dart
@@ -3,52 +3,169 @@ import 'package:group_plus/api.dart';
 import 'package:group_plus/response.dart';
 
 //Classes
-class SignUpPage extends StatefulWidget {
-  const SignUpPage({Key? key, required this.title }) : super (key: key);
-  final String title;
-
-  @override
-  State<SignUpPage> createState()=> _SignUpPageState();
-}
-
-class _SignUpPageState extends State<SignUpPage>{
 
   // Controllers
 
+class SignUpPage extends StatefulWidget {
+    const SignUpPage({Key? key, required this.title }) : super (key: key);
+  final String title;
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+   Widget build(BuildContext context) {
+     
+    return Scaffold(
+     backgroundColor: Colors.blueGrey.shade100,
+      body: Container(
+         padding: EdgeInsets.all(50.0),
+            child: Column(
+                children: <Widget>[
+                  /*
+                  SizedBox(height: 200.0,child:Image.asset("",fit: BoxFit.contain,) ),
+                  */           
+                                                      TextField(
+                    autocorrect:false,
+                    textInputAction: TextInputAction.unspecified,
+                    controller: null,
+                    textAlign: TextAlign.right,
+                    textDirection: TextDirection.ltr,
+                    autofocus: true,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(Icons.email,color: Colors.black26,size: 20,),
+                      hintText: "الإسم",
+                      labelText: " الإسم ",
+                      border: OutlineInputBorder(borderRadius:BorderRadius.circular(10))
+                    
+                    ),
+                  ),
+                  SizedBox(height: 30.0,),
+
+                  TextField(
+                    autocorrect:false,
+                    textInputAction: TextInputAction.unspecified,
+                    controller: null,
+                    textAlign: TextAlign.right,
+                    textDirection: TextDirection.ltr,
+                    autofocus: true,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(Icons.email,color: Colors.black26,size: 20,),
+                      hintText: "البريد الالكتروني",
+                      labelText: "البريد الالكتروني ",
+                      border: OutlineInputBorder(borderRadius:BorderRadius.circular(10))
+                    
+                    ),
+                    /*
+                    style: TextStyle(
+                      color: Colors.pinkAccent,
+                    ),
+                    */
+                  ),
+
+                  SizedBox(height: 30.0,),
 
 
+                  TextField(
+                    obscureText: true,
+                    autocorrect:false,
+                    textInputAction: TextInputAction.unspecified,
+                    controller: null,
+                    textAlign: TextAlign.right,
+                    textDirection: TextDirection.ltr,
+                    autofocus: true,
+                    maxLength: 10,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(Icons.https,color: Colors.black26,size: 20,),
+                      hintText: "كلمة المرور",
+                      labelText: "كلمة المرور",
+                      border: OutlineInputBorder()
+                    ),
+                    /*
+                    style: TextStyle(
+                      color: Colors.pinkAccent,
+                    ),
+                    */
 
+                  ),
+                  SizedBox(height: 10.0,),
+                  TextField(
+                    obscureText: true,
+                    autocorrect:false,
+                    textInputAction: TextInputAction.unspecified,
+                    controller: null,
+                    textAlign: TextAlign.right,
+                    textDirection: TextDirection.ltr,
+                    autofocus: true,
+                    maxLength: 10,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(Icons.https,color: Colors.black26,size: 20,),
+                      hintText: "إعادة كلمة المرور" ,
+                      labelText: "إعادة كلمة المرور",
+                      border: OutlineInputBorder()
+                    ),
+                    /*
+                    style: TextStyle(
+                      color: Colors.pinkAccent,
+                    ),
+                    */
 
+                  ),
+                  SizedBox(height: 10.0,),
+                  TextField(
+                    obscureText: true,
+                    autocorrect:false,
+                    textInputAction: TextInputAction.unspecified,
+                    controller: null,
+                    textAlign: TextAlign.right,
+                    textDirection: TextDirection.rtl,
+                    autofocus: true,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(Icons.add_location,color: Colors.black26,size: 20,),
+                      hintText: "المدينة" ,
+                      labelText: "المدينة",                      border: OutlineInputBorder()
+                    ),
+                    /*
+                    style: TextStyle(
+                      color: Colors.pinkAccent,
+                    ),
+                    */
+                     ),
 
+                    SizedBox(height: 30.0,),
 
-  // App interface
-@override
-Widget build(BuildContext context){
-  return Scaffold(
-      backgroundColor: Colors.white70,
-      body: SafeArea(
-      child: Center(
-      child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-  SizedBox(height: 20),
-  Text(
-  'Sign Up Screen',
-  style: TextStyle(fontSize: 35, color: Colors.blueGrey[400]),
-  ),
-  SizedBox(height: 20),
-
-        //Sign up form
-
-
-
-
-
-
-
-
-      ],),),),);
-}}
+                  SizedBox(height: 200.0,
+                  
+                  child:
+                  SizedBox(height: 100.0,width: 100,
+                  child:
+                  Text("تسجيل الدخول",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black12,
+                   fontWeight:FontWeight.bold,
+                   backgroundColor:Colors.deepOrange)) ,) )
+                  
+      
+                  
+                ]
+              
+            )
+            
+            
+  
+      ),
+      
+    );
+  }
+}
 
 //Sign up button
 //On press > Get values from TextFields using controllers.
