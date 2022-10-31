20413207275f7398e89f4f58fdd6833ad29b3c3b
salwa alqurashi <92172130+Salwaq@users.noreply.github.com>
2022-10-31 01:39:47 -0700

    import AbstractBaseUser

--- django_project/register/models.py
+++ django_project/register/models.py
@@ -1,7 +1,7 @@
 from enum import unique
 from unittest.util import _MAX_LENGTH
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
 class User(AbstractBaseUser): 
